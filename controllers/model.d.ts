a45a726184ff39b04493cb1ef20082135b08aae3
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2020-04-17 12:46:56 +0300

    initial commit

---
+++ controllers/model.d.ts
@@ -0,0 +1,16 @@
+import {HttpController} from "../mvc";
+
+declare class HttpDataModelController extends HttpController{
+    public getItem():Promise<any>;
+    public showItem():Promise<any>;
+    public postItem():Promise<any>;
+    public deleteItem():Promise<any>;
+    public createItem():Promise<any>;
+    public getItems():Promise<any>;
+    public postItems():Promise<any>;
+    public putItems():Promise<any>;
+    public deleteItems():Promise<any>;
+
+}
+
+export default HttpDataModelController;
